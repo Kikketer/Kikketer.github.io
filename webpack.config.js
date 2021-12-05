493abba6318ec7c648f4785c759bb449bb9ae486
Chris Weed <chris@cjweed.com>
2021-12-05 11:51:29 -0600

    Removed main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
 
-const isDev = process.env.ELEVENTY_ENV === 'development';
+const isDev = process.env.ELEVENTY_ENV === 'development'
 
-const baseFilename = isDev ? 'main' : 'main.[contenthash]';
+const baseFilename = isDev ? 'main' : 'main.[contenthash]'
 
 module.exports = {
   entry: [
-    path.resolve(__dirname, 'src', 'js', 'main.js'),
+    // path.resolve(__dirname, 'src', 'js', 'main.js'),
     path.resolve(__dirname, 'src', 'css', 'main.css'),
   ],
   output: {
@@ -56,4 +56,4 @@ module.exports = {
     new WebpackManifestPlugin({ publicPath: '/assets/' }),
     new MiniCssExtractPlugin({ filename: `${baseFilename}.css` }),
   ],
-};
+}
